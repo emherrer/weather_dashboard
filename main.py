dc115777ef9117275f965322560b67dd7106a00b
Emmanuel Herrera <emherrer1@gmail.com>
2023-07-10 17:48:45 -0400

    add sky labels, language and error mesages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 import json
 from backend import get_data
+from datetime import date
 
 
 # Add page config and titles
@@ -9,43 +10,55 @@ st.set_page_config(page_title="Home",
                    page_icon="🗺",
                    layout="centered")
 
-st.title("Weather Forecast for the Next Days")
+st.title("Pronóstico del Tiempo para los Próximos Días")
 
 # Add country codes box
 with open("countries.json", "r") as file:
     content = file.read()
 country_codes = json.loads(content)
 country = st.selectbox(
-    "Select country", ([key for key in country_codes.keys()]))
+    "Seleccione un país", ([key for key in country_codes.keys()]))
 
 # Add text input, slider, select box and subheader
-place = st.text_input("Place: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5,
-                 help="Select the number of forecasted days")
-option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place} {country}")
-
+place = st.text_input("Lugar: ", help="Ejemplo: Santiago")
+days = st.slider("Días pronosticados", min_value=1, max_value=5,
+                 help="Seleccione el número de días para el pronóstico")
+option = st.selectbox("Seleccione el formato del pronóstico", ("Temperatura", "Cielos"))
+st.subheader(f"{option} para los próximos {days} días en {place} {country}")
 
 if place:
-    # Get filtered data
-    filtered_data = get_data(country=country, place=place, forecast=days)
+    try:
+        # Get filtered data
+        filtered_data = get_data(country=country, place=place, forecast=days)
 
-    if option == "Temperature":
-        temperatures = [temp.get("main").get("temp")
-                        for temp in filtered_data]
-        dates = [date.get("dt_txt") for date in filtered_data]
+        if option == "Temperatura":
+            temperatures = [temp.get("main").get("temp")
+                            for temp in filtered_data]
+            dates = [date.get("dt_txt") for date in filtered_data]
 
-        # Create Temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={
-            "x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+            # Create Temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={
+                "x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [sky.get("weather")[0].get("main")
-                          for sky in filtered_data]
+        if option == "Cielos":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            dates = [date.get("dt_txt") for date in filtered_data]
+            sky_conditions = [sky.get("weather")[0].get("main")
+                              for sky in filtered_data]
 
-        # Create Skys plots
-        image_paths = [images.get(sky) for sky in sky_conditions]
-        st.image(image_paths, width=100)
+            # Create Skys plots
+            image_paths = [images.get(sky) for sky in sky_conditions]
+
+            # Code for the image labels (must be improve)
+            img_label = [date(day=int(dates[i][8:10]),
+                              month=int(dates[i][5:7]),
+                              year=int(dates[i][0:4])).
+                         strftime(f'%a, %b %d {dates[i][11:-3]}')
+                         for i in range(days*8)]
+
+            st.image(image_paths, width=100, caption=img_label)
+    except TypeError:
+        st.error(
+            "You entered a place that does not exist, please try again!!", icon="🚨")
